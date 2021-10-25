f98b0cd35672fbae23f86f512f8469fef72dcc91
Tom Boehmer <rastus2203@gmail.com>
2021-10-25 19:25:03 +1000

    initial commit

---
+++ bungee.m
@@ -0,0 +1,9 @@
+% Model Parameters
+JumpPointHeight = 74;       % Height of Jump Point              : m
+DeckHeight = 31;            % Deck Height                       : m
+DragCoefficient = 0.9;      % Drag Coefficient                  : kg/m
+HumanMass = 80;             % Mass of Jumper                    : kg
+RopeLength = 25;            % Length of Bungee Rope             : m
+RopeSpringConstant = 90;    % Spring Constant of Bungee Rope    : N/m
+g = 9.8;                    % Gravitational Acceleration        : m/s^2
+
